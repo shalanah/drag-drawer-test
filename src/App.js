ed2d688814ce3a7f5f23debb67a2128231408fb9
shalanah <shalanahfaith@gmail.com>
2020-10-12 14:50:54 -0500

    More changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import DragDrawer from './DragDrawer'
 
@@ -8,32 +8,50 @@ const MainContainer = styled.div`
   background: #efefef;
   padding: 20px;
 `
+const closedHeight = 120
+
+const DragDrawerContent = () => {
+  return (
+    <div
+      style={{
+        padding: '20px',
+        height: '200vh',
+        background:
+          'linear-gradient(#fff, #fff, red, orange, yellow, green, cyan, blue)'
+      }}
+    >
+      Scrollable content
+    </div>
+  )
+}
+const DragDrawerFooter = () => {
+  return <div style={{ height: 60, padding: '20px' }}>Footer buttons</div>
+}
 
 const App = () => {
+  const [winHeight, setWinHeight] = useState(window.innerHeight)
+  useEffect(() => {
+    const handleResize = () => {
+      setWinHeight(window.innerHeight)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [setWinHeight])
   return (
-    <MainContainer>
-      <span>App content</span>
+    <>
+      <MainContainer>
+        <span>App content</span>
+      </MainContainer>
       <DragDrawer
         style={{ background: '#fff', zIndex: 100 }}
-        closedHeight={110}
-        dragElem={<div style={{ padding: '20px', height: 110 }}>Pull here</div>}
-        content={
-          <div
-            style={{
-              padding: '0px 20px',
-              height: '200vh',
-              background:
-                'linear-gradient(#fff, red, orange, yellow, green, cyan, blue)'
-            }}
-          >
-            Scrollable content
-          </div>
-        }
-        footer={
-          <div style={{ height: 60, padding: '20px' }}>Footer buttons</div>
-        }
+        openHeight={winHeight - 200}
+        closedHeight={closedHeight}
+        content={<DragDrawerContent />}
+        footer={<DragDrawerFooter />}
       />
-    </MainContainer>
+    </>
   )
 }
 
--- src/DragDrawer.js
+++ src/DragDrawer.js
@@ -3,7 +3,6 @@ import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 import styled from 'styled-components'
 
-const NAV_PERCENT = 0.8
 const Container = styled(animated.div)`
   position: fixed;
   width: 100%;
@@ -11,43 +10,45 @@ const Container = styled(animated.div)`
   flex-direction: column;
   bottom: 0px;
   left: 0;
-  border-radius: 10px 10px 0 0;
+`
+const clamp = (num, min, max) => Math.max(Math.min(max, num), min)
+const config = {
+  mass: 1,
+  tension: 400,
+  clamp: true,
+  friction: 35 /* since we are mostly clamping, want to close pretty quickly */
+}
+const Handle = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background-color: #ccc;
+  transition: 0.05s;
+  :after {
+    position: absolute;
+    content: '';
+    width: 100px;
+    height: 8px;
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    background: #fff;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 `
 
-const clamp = (num, min, max) => Math.max(Math.min(max, num), min)
-const dragClassName = 'drag-drawer-container'
-const draggingOnHandle = (element) => {
-  let dragElems = document.getElementsByClassName(dragClassName)
-  let isDragging = false
-  const len = dragElems.length
-  for (let i = 0; i < len; i++) {
-    let dragElem = dragElems[i]
-    isDragging = dragElem === element || dragElem.contains(element)
-    dragElem = null // release to be overly cautious
-    if (isDragging) break
-  }
-  dragElems = null // release to be overly cautious
-  return isDragging
-}
-
 const handleDrag = ({
-  refContainer,
-  openHeight,
-  closedHeight,
-  set,
-  windowHeight,
-  setOpen,
-  open,
-  config,
-  y0,
-  // args
-  event,
+  args: [
+    { refScrollContainer, refContainer, openY, closeY, set, setOpen, open }
+  ],
+  memo,
   vxvy: [vx, vy],
   movement: [mx, my],
   cancel,
-  first,
-  last,
-  refScrollContainer
+  last
 }) => {
   if (
     refScrollContainer?.current &&
@@ -56,38 +57,32 @@ const handleDrag = ({
     // Do not go up or down if scroll area isn't at top
     cancel()
   }
-  if (event.persist) {
-    event.persist()
-    if (event.target && !draggingOnHandle(event.target)) {
-      cancel()
-    }
-  }
-  if (first)
-    y0.current = Number(
+  if (!memo) {
+    // Get current Y translation
+    memo = Number(
       refContainer.current.style.transform.split('(')[1].split('px')[0]
     )
-  const openY = 0
-  const closeY = openHeight - closedHeight
-  const min = openY
-  const max = openHeight - 20 // don't want to go below screen
-  let immediate = true
-  let y = clamp(my + y0.current, min, max)
-  let velocity = 0
+  }
+  let immediate = true // will not animate (follows finger)
+  let y = clamp(my + memo, openY, closeY)
+  let velocity = 0 // default
+  let onRest
+
   // Snapping to open or closed
   if (last) {
     immediate = false // will animate
     velocity = vy
-    const threshold = clamp(windowHeight * 0.1, 5, 300)
+    const threshold = clamp(window.innerHeight * 0.1, 5, 300) // do we really need window height?
     if (open) {
       if (y > openY + threshold) {
-        setOpen(false)
+        onRest = () => setOpen(false)
         y = closeY
       } else {
         y = openY
       }
     } else {
       if (y < closeY - threshold) {
-        setOpen(true)
+        onRest = () => setOpen(true)
         y = openY
       } else y = closeY
     }
@@ -96,15 +91,17 @@ const handleDrag = ({
     y,
     config: {
       ...config,
-      velocity,
-      clamp: y < closeY
+      velocity
     },
-    immediate
+    immediate,
+    onRest
   })
+  return memo // start position for memo
 }
 
 const DragDrawer = ({
   content,
+  openHeight,
   closedHeight,
   onChange,
   dragElem,
@@ -115,60 +112,79 @@ const DragDrawer = ({
   const [open, setOpen] = useState(false)
   const refContainer = useRef()
   const refScrollContainer = useRef()
-  const windowHeight = window.innerHeight
-  const openHeight = windowHeight * NAV_PERCENT
-
-  const config = {
-    mass: 1,
-    tension: 400,
-    friction: 35 /* since we are mostly clamping, want to close pretty quickly */
-  }
+  const openY = 0
+  const closeY = openHeight - closedHeight
+  // Default behavior
   const [animProps, set] = useSpring(() => ({
     y: openHeight - closedHeight,
-    immediate: true,
+    immediate: true, // no animation, just following finger
     config
   }))
-
-  const y0 = useRef()
-  const dragEvents = useDrag((useDragArgs) =>
-    handleDrag({
-      ...useDragArgs,
-      refContainer,
-      openHeight,
-      closedHeight,
-      set,
-      windowHeight,
-      setOpen,
-      y0,
-      open,
-      config,
-      refScrollContainer
-    })
-  )
-
+  const dragEvents = useDrag(handleDrag)
+  // Click outside when open will close drawer
+  useEffect(() => {
+    const closeDrawer = (e) => {
+      if (open && refContainer && !refContainer.current.contains(e.target)) {
+        if (refScrollContainer?.current) {
+          refScrollContainer.current.scrollTop = 0
+        }
+        set({
+          y: closeY,
+          config,
+          immediate: false,
+          onRest: () => {
+            setOpen(false)
+          }
+        })
+      }
+    }
+    window.addEventListener('click', closeDrawer)
+    return () => {
+      window.removeEventListener('click', closeDrawer)
+    }
+  }, [open, refContainer, refScrollContainer, closeY, set])
   return (
     <Container
+      onClick={() => {
+        if (!open) {
+          set({
+            y: openY,
+            config,
+            immediate: false,
+            onRest: () => {
+              setOpen(true)
+            }
+          })
+        }
+      }}
       ref={refContainer}
       style={{
         height: openHeight,
         transform: animProps.y.to((y) => `translateY(${y}px)`),
         ...style
       }}
-      {...dragEvents()}
       {...props}
     >
-      <div className={dragClassName}>{dragElem}</div>
       <div
+        {...dragEvents({
+          refContainer,
+          openY,
+          closeY,
+          set,
+          setOpen,
+          open,
+          refScrollContainer
+        })}
         ref={refScrollContainer}
-        className={dragClassName}
         style={{
+          scrollBehavior: 'smooth', // for when we need to force back to the top
           overflowY: 'scroll',
           flex: 1
         }}
-        onPointerMove={(e) => {
-          e.preventDefault() // try to stop safari bounce
-        }}
       >
+        <Handle
+          style={{ opacity: open ? 0 : 1, pointerEvents: open ? 'none' : '' }}
+        />
         {content}
       </div>
       {footer && <div>{footer}</div>}
